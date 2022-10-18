18d4e4f59e7d296476c7f3d0baeecd6c32fcf254
lantrannk <115966747+lantrannk@users.noreply.github.com>
2022-10-18 10:39:41 +0700

    Add files via upload

---
+++ dart/chapter2/mini-chapter2.dart
@@ -0,0 +1,17 @@
+void main(List<String> args) {
+  
+  // declare a constant of type int
+  const myAge = 21;
+
+  // declare a variable of type double and change it
+  double averageAge = 21;
+  averageAge = (21 + 22) / 2;
+  print(averageAge);
+
+  // declare const testNumber and const evenOdd equal to testNumber modulo 2.
+  // testNumber change => evenOdd change
+  const testNumber = 75;
+  const evenOdd = testNumber % 2;
+  print(evenOdd);
+
+}
