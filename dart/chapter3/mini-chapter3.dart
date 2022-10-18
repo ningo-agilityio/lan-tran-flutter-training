59fdc2fafdae9a249bb272bf5a2ed53f364e11fc
lantrannk <115966747+lantrannk@users.noreply.github.com>
2022-10-17 21:16:20 -0700

    Create mini-chapter3.dart

---
+++ dart/chapter3/mini-chapter3.dart
@@ -0,0 +1,15 @@
+void main(List<String> args) {
+
+  // type number
+  const age1 = 42;
+  const age2 = 21;
+  const averageAge = (age1 + age2) /2;
+  print(averageAge);
+
+  // type string
+  const firstName = 'Lan';
+  const lastName = 'Tran';
+  const fullName = firstName + ' ' + lastName;
+  const myDetails = 'Hello, my name is $fullName.';
+  print(myDetails);
+}
