1fe1486eb11e4ce70289c2eb4dbf3ebae2b50978
lantrannk <lantrannk@gmail.com>
2022-11-01 10:37:18 +0700

    dart practice day 2

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 # Dart Practice
 ---
+Run in terminal.
 Run command: dart /bin/users.dart
 ---
 > Functions:
+- Check to continue the program.
+- Read users list: Display all users list on terminal.
 - Create a user: Enter informations in the form.
+- Update a user: Enter the id of user that you want to update. Then enter the property that you want to change and the changed information. Dart will update that user and display on terminal.
 - Delete a user: Enter an id. Dart will delete the user has that id if it exists.
 - Find a user by id: Enter the id that you want to find. Dart will show you the user has that id if it exists.
 - Find a user by name: Enter the name that you want to find. Dart will show you the user has that name if it exists.
--- bin/users.dart
+++ bin/users.dart
@@ -4,7 +4,8 @@ import 'dart:io';
 import 'dart:math';
 import 'dart:isolate';
 
-List<String> rolesList = ["Editor", "Author", "Maintainer", "Subcriber"];
+List<String> rolesList = ["Editor", "Author", "Maintainer", "Subscriber"];
+var continueProgram = true;
 
 void main(List<String> args) {
   final user = Users( address: "7777 Mendez Plains",
@@ -12,31 +13,38 @@ void main(List<String> args) {
     email: "don85@johnson.com",
     phone: "(616) 865-4180",
     country: "USA",
-    dateOfBirth: DateTime.utc(1988,5,23),
+    dateOfBirth: DateTime.parse('19880523'),
     id: Random().nextInt(10000),
     name: "Jordan Stevenson",
     role: "Editor",
   );
 
   List<Users> users = [user];
-  createUser(users);
-  print('---');
-  print(users);
-  print('---');
-  findById(users);
-  print('---');
-  findByName(users);
-  print('---');
-  findByEmail(users);
-  print('---');
-  print('Filter:');
-  filterRole(users);
-  print('---');
-  print('Delete User:');
-  deleteUser(users);
-  print(users);
+
+  while (continueProgram) {
+    createUser(users);
+    print('---');
+    readUsers(users);
+    print('---');
+    updateUser(users);
+    print('---');
+    findById(users);
+    print('---');
+    findByName(users);
+    print('---');
+    findByEmail(users);
+    print('---');
+    print('Filter:');
+    filterRole(users);
+    print('---');
+    // print('Delete User:');
+    // deleteUser(users);
+    // print(users);
+    continueProgramming();
+  } 
 }
 
+// data class
 class Users {
   String address, company, email, phone, country, name, role;
   DateTime dateOfBirth;
@@ -70,7 +78,7 @@ class Users {
   @override
   String toString() {
     // TODO: implement toString
-    return '\nUser:'
+    return '\n\nUser:'
         '\naddress: $address'
         '\ncompany: $company'
         '\nemail: $email'
@@ -79,10 +87,35 @@ class Users {
         '\ndateOfBirth: $dateOfBirth'
         '\nid: $id'
         '\nname: $name'
-        '\nrole: $role';
+        '\nrole: $role\n';
   }
 }
 
+// function: check to continue
+void continueProgramming() {
+  print('Do you want to continue? (Y/n)');
+  var hint = stdin.readLineSync();
+  switch(hint) {
+    case 'Y': 
+    case 'y':
+      continueProgram = true;
+      break;
+    case 'n':
+      continueProgram = false;
+      break;
+    default:
+      continueProgram = false;
+      print('Error!');
+      break;
+  }
+}
+
+// function: print all users
+void readUsers(List<Users> users) {
+  return print(users);
+}
+
+// function: create a user in form
 void createUser(List<Users> users) {
   var user = Users(
     address: "7777 Mendez Plains",
@@ -90,12 +123,13 @@ void createUser(List<Users> users) {
     email: "don85@johnson.com",
     phone: "(616) 865-4180",
     country: "USA",
-    dateOfBirth: DateTime.parse('1988-05-23'),
+    dateOfBirth: DateTime.parse('19880523'),
     id: Random().nextInt(10000),
     name: "Jordan Stevenson",
     role: "Editor",
   );
 
+  // enter information of a user
   print('Create a user:\n');
   stdout.write('Address: ');
   String? address = stdin.readLineSync();
@@ -107,19 +141,20 @@ void createUser(List<Users> users) {
   String? phone = stdin.readLineSync();
   stdout.write('Country: ');
   String? country = stdin.readLineSync();
-  stdout.write('Date of birth: ');
+  stdout.write('Date of birth (Format: YYYYMMDD): ');
   String? dateOfBirth = stdin.readLineSync();
   stdout.write('Name: ');
   String? name= stdin.readLineSync();
   stdout.write('Role: ');
   String? role = stdin.readLineSync();
 
+  // add values to variable 'user'
   user.address = address!;
   user.company = company!;
   user.email = email!;
   user.phone = phone!;
   user.country = country!;
-  user.dateOfBirth = DateTime.parse(dateOfBirth!);
+  user.dateOfBirth =  DateTime.parse(dateOfBirth!);
   user.name = name!;
   if (rolesList.contains(role))
     user.role = role!;
@@ -127,51 +162,117 @@ void createUser(List<Users> users) {
     user.role = 'null';
   }
 
-  return users.add(user);
+  return users.add(user); // add a user to users list
 }
 
+// function: update an information of a user
+void updateUser(List<Users> users) {
+  print('Which user do you want to update?');
+  print('Update ID: ');
+  int? updateId = int.parse(stdin.readLineSync()!);
+  Users user = users.firstWhere((user) => user.id == updateId);
+  print('\nWhich property do you want to update?');
+  print(
+    '1. Address\n'
+    '2. Company\n'
+    '3. Email\n'
+    '4. Phone\n'
+    '5. Country\n'
+    '6. Date Of Birth\n'
+    '7. Name\n'
+    '8. Role\n'
+    '0. Exit\n'
+  );
+
+  int? select = int.parse(stdin.readLineSync()!);
+  switch(select) {
+    case 1:
+      print('\nAddress: ');
+      user.address = stdin.readLineSync()!;
+      break;
+    case 2:
+      print('\nCompany: ');
+      user.company = stdin.readLineSync()!;
+      break;
+    case 3:
+      print('\nEmail: ');
+      user.email = stdin.readLineSync()!;
+      break;
+    case 4:
+      print('\nPhone: ');
+      user.phone = stdin.readLineSync()!;
+      break;
+    case 5:
+      print('\nCountry: ');
+      user.country = stdin.readLineSync()!;
+      break;
+    case 6:
+      print('\nDate Of Birth: ');
+      user.dateOfBirth = DateTime.parse(stdin.readLineSync()!);
+      break;
+    case 7:
+      print('\nName: ');
+      user.name = stdin.readLineSync()!;
+      break;
+    case 8:
+      print('\nRole (Editor, Author, Maintainer, Subscriber): ');
+      user.role = stdin.readLineSync()!;
+      break;
+    case 0:
+      print('\nClosed.');
+      break;
+  }
+  
+  return print(user);
+}
+
+// function: delete a user by id
 void deleteUser(List<Users> users) {
   stdout.write('Find By ID: ');
   int? deleteId = int.parse(stdin.readLineSync()!);
 
-  return users.removeWhere((user) => user.id == deleteId);
+  return users.removeWhere((user) => user.id == deleteId); // delete the user has the input id
 }
 
+// function: find a user by id
 void findById(List<Users> users) {
   stdout.write('Find By ID: ');
   int? findId = int.parse(stdin.readLineSync()!);
 
   for (var user in users) {
-    if (user.id == findId) return print(user);
+    if (user.id == findId) return print(user); // print the user has the input id
   }
   return print('User not found.');
 }
 
+// find a user by name
 void findByName(List<Users> users) {
   stdout.write('Find By Name: ');
   String? findName = stdin.readLineSync();
 
   for (var user in users) {
-    if (user.name.contains(findName!)) return print(user);
+    if (user.name.contains(findName!)) return print(user); // print the user has the input name
   }
   return print('User not found.');
 }
 
+// find a user by email
 void findByEmail(List<Users> users) {
   stdout.write('Find By Email: ');
   String? findEmail = stdin.readLineSync();
 
   for (var user in users) {
-    if (user.email.contains(findEmail!)) return print(user);
+    if (user.email.contains(findEmail!)) return print(user); // print the user has the input email
   }
   return print('User not found.');
 }
 
+// function: filter users by roles
 void filterRole(List<Users> users) {
   stdout.write('Filter By Role: ');
   String? findRole = stdin.readLineSync();
 
   users.forEach((user) {
-    if (user.role == findRole!) return print(user);
+    if (user.role == findRole!) return print(user); // print every user has the input role
   });
 }
