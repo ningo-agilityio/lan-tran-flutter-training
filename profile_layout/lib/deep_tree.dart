47e1ad72b8f36c4e33269ea00c4be6b496d72178
lantrannk <lantrannk@gmail.com>
2022-11-21 16:41:55 +0700

    finished chapter04

---
+++ profile_layout/lib/deep_tree.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class DeepTree extends StatelessWidget {
+  const DeepTree({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  FlutterLogo(),
+                  Text('Flutter is amazing.'),
+                ],
+              ),
+              Expanded(
+                child: Container(
+                  color: Colors.purple,
+                ),
+              ),
+              Text('Its all widgets!'),
+              Text('Let\'s find out how deep the rabbit hole goes.'),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- profile_layout/lib/main.dart
+++ profile_layout/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:profile_layout/flex_screen.dart';
 import 'package:profile_layout/profile_screen.dart';
+import 'deep_tree.dart';
+import 'e_commerce_screen_before.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +15,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: ProfileScreen(),
+      theme: ThemeData(
+        brightness: Brightness.light,
+        primaryColor: Colors.green,
+        appBarTheme: const AppBarTheme(
+          elevation: 10,
+          textTheme: TextTheme(
+            headline6: TextStyle(
+              fontFamily: 'LeckerliOne',
+              fontSize: 24,
+            ),
+          ),
+        ),
+      ),
+      home: ECommerceScreen(),
     );
   }
 }
