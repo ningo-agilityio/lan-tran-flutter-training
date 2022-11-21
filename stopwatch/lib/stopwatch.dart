ff147382b45398eb8316da51f9c83a166f865d1b
lantrannk <lantrannk@gmail.com>
2022-11-21 16:42:13 +0700

    reading chapter05

---
+++ stopwatch/lib/stopwatch.dart
@@ -0,0 +1,47 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+class StopWatch extends StatefulWidget {
+  @override
+  State createState() => StopWatchState();
+}
+
+class StopWatchState extends State<StopWatch> {
+  late int seconds;
+  late Timer timer;
+  @override
+  void initState() {
+    super.initState();
+    seconds = 0;
+    timer = Timer.periodic(Duration(seconds: 1), _onTick);
+  }
+
+  void _onTick(Timer time) {
+    setState(() {
+      ++seconds;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Stopwatch'),
+      ),
+      body: Center(
+        child: Text(
+          '$seconds ${_secondsText()}',
+          style: Theme.of(context).textTheme.headline5,
+        ),
+      ),
+    );
+  }
+
+  String _secondsText() => seconds == 1 ? 'second' : 'seconds';
+
+  @override
+  void dispose() {
+    timer.cancel();
+    super.dispose();
+  }
+}
