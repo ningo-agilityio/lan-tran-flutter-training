d0111a31cc62d1ded5282aa862b75f3b63e9dda6
lantrannk <lantrannk@gmail.com>
2022-12-26 17:18:06 +0700

    add widget login-button

--- lib/widgets/login_button.dart
+++ lib/widgets/login_button.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:salon_appointment/screens/calendar.dart';
+import 'package:salon_appointment/screens/calendar_screen.dart';
 
 class LoginButton extends StatelessWidget {
   const LoginButton({super.key});
@@ -7,6 +7,12 @@ class LoginButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: Color(0xFFFFFFFF),
+        ),
+        borderRadius: BorderRadius.circular(24.0),
+      ),
       child: ElevatedButton(
         style: ButtonStyle(
           fixedSize: MaterialStateProperty.all(Size(311, 44)),
@@ -14,18 +20,18 @@ class LoginButton extends StatelessWidget {
             RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(24.0),
               side: const BorderSide(
-                width: 10.0,
-                color: Colors.white,
+                width: 0.0,
               ),
             ),
           ),
+          backgroundColor: MaterialStateProperty.all(Color(0x00FFFFFF)),
           alignment: Alignment.center,
         ),
         onPressed: () {
           Navigator.pushReplacement(
             context,
             MaterialPageRoute(
-              builder: (context) => Calendar(),
+              builder: (context) => CalendarScreen(),
             ),
           );
         },
@@ -35,6 +41,7 @@ class LoginButton extends StatelessWidget {
             fontSize: 16,
             fontFamily: 'Poppins',
             color: Color(0xFFFFFFFF),
+            backgroundColor: Color(0xFFFFFF),
           ),
         ),
       ),
