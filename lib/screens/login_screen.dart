8f6bbb5ffca4a4773507ad5bf50b28712d989183
lantrannk <lantrannk@gmail.com>
2023-03-15 16:54:55 +0700

    create login validation

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -6,7 +6,6 @@ import 'package:salon_appointment/widgets/common/text.dart';
 import 'package:salon_appointment/widgets/input.dart';
 import 'package:salon_appointment/widgets/forget_password.dart';
 import 'package:salon_appointment/widgets/login_button.dart';
-import 'package:salon_appointment/screens/calendar_screen.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -16,6 +15,11 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  String? phoneNumberErrorText = '';
+  String? passwordErrorText = '';
+  String phoneNumber = '';
+  String password = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,45 +28,124 @@ class _LoginScreenState extends State<LoginScreen> {
           BackgroundImage(),
           Container(
             margin: EdgeInsets.all(32.0),
-            child: Column(
-              children: [
-                Padding(
-                  padding: EdgeInsets.only(
-                    top: 60.0,
-                    bottom: 147.0,
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(
+                      top: 60.0,
+                      bottom: 147.0,
+                    ),
+                    child: SACustomText.logoText,
                   ),
-                  child: SACustomText.logoText,
-                ),
-                Input(
-                  text: 'Phone number',
-                  keyboardType: TextInputType.number,
-                  color: SATheme.lightTheme.colorScheme.secondary,
-                ),
-                SizedBox(height: 16),
-                Input(
-                  text: 'Password',
-                  color: SATheme.lightTheme.colorScheme.secondary,
-                ),
-                SizedBox(height: 16),
-                ForgetPassword(
-                  onPress: () {},
-                ),
-                SizedBox(height: 24),
-                LoginButton(
-                  onPress: () {
-                    Navigator.pushReplacement(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => MainScaffold(),
-                      ),
-                    );
-                  },
-                ),
-              ],
+                  SizedBox(
+                    height: 72.0,
+                    child: Input(
+                      text: 'Phone number',
+                      keyboardType: TextInputType.phone,
+                      color: SATheme.lightTheme.colorScheme.secondary,
+                      errorText: phoneNumberErrorText == ''
+                          ? null
+                          : phoneNumberErrorText,
+                      validator: (value) {
+                        return isValidPhoneNumber(value!);
+                      },
+                      onChanged: (value) {
+                        setState(() {
+                          phoneNumberErrorText = isValidPhoneNumber(value);
+                          phoneNumber = value;
+                        });
+                      },
+                    ),
+                  ),
+                  SizedBox(height: 16),
+                  SizedBox(
+                    height: 72.0,
+                    child: Input(
+                      text: 'Password',
+                      color: SATheme.lightTheme.colorScheme.secondary,
+                      errorText:
+                          passwordErrorText == '' ? null : passwordErrorText,
+                      validator: (value) {
+                        return isValidPassword(value!);
+                      },
+                      onChanged: (value) {
+                        setState(() {
+                          passwordErrorText = isValidPassword(value);
+                          password = value;
+                        });
+                      },
+                    ),
+                  ),
+                  SizedBox(height: 16),
+                  ForgetPassword(
+                    onPress: () {},
+                  ),
+                  SizedBox(height: 24),
+                  LoginButton(onPress: () {
+                    disableLoginButton()
+                        ? showSnackBar(Text('Information is invalid.'))
+                        : isLoginSuccess();
+                  }),
+                ],
+              ),
             ),
           ),
         ],
       ),
     );
   }
+
+  String? isValidPhoneNumber(String? phoneNumber) {
+    if (phoneNumber == '') {
+      return 'Phone number is blank.';
+    } else if (phoneNumber!.length != 10) {
+      return 'Phone number is invalid.';
+    }
+    return null;
+  }
+
+  String? isValidPassword(String? password) {
+    if (password == '') {
+      return 'Password is blank.';
+    } else if (password!.length < 6) {
+      return 'Password must be at least 6 characters.';
+    }
+    return null;
+  }
+
+  bool disableLoginButton() {
+    return isValidPassword(password) != null ||
+        isValidPhoneNumber(phoneNumber) != null;
+  }
+
+  void isLoginSuccess() {
+    for (var user in users) {
+      if (!user.containsValue(phoneNumber)) {
+        showSnackBar(Text('Phone number is not exist.'));
+      } else if (!user.containsValue(password)) {
+        showSnackBar(Text('Password is incorrect.'));
+      } else {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => MainScaffold(),
+          ),
+        );
+      }
+    }
+  }
+
+  void showSnackBar(Widget content) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        duration: const Duration(seconds: 1),
+        content: content,
+      ),
+    );
+  }
+
+  List<Map<String, String>> users = [
+    {'phoneNumber': '0905123456', 'password': '111111'},
+  ];
 }
--- lib/widgets/input.dart
+++ lib/widgets/input.dart
@@ -1,40 +1,60 @@
 import 'package:flutter/material.dart';
 import 'package:salon_appointment/theme/theme.dart';
 
-class Input extends StatelessWidget {
-  String text;
-  final TextInputType? keyboardType;
-  final _textController = TextEditingController();
-  final Color? color;
+class Input extends StatefulWidget {
   Input({
     super.key,
     required this.text,
     this.keyboardType,
     this.color,
+    this.onChanged,
+    this.validator,
+    this.errorText,
   });
+
+  String text;
+  final TextInputType? keyboardType;
+  final Color? color;
+  final Function(String)? onChanged;
+  String? Function(String?)? validator;
+  final String? errorText;
+
+  @override
+  State<Input> createState() => _InputState();
+}
+
+class _InputState extends State<Input> {
+  final _textController = TextEditingController();
+
+  final _focusNode = FocusNode();
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 44,
-      width: 311,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(8.0),
-        color: Theme.of(context).colorScheme.secondary.withOpacity(0.235),
-      ),
+    return SizedBox(
       child: TextFormField(
         controller: _textController,
-        keyboardType: keyboardType,
+        enableInteractiveSelection: true,
+        focusNode: _focusNode,
+        keyboardType: widget.keyboardType,
+        style: TextStyle(color: SATheme.lightTheme.colorScheme.secondary),
         decoration: InputDecoration(
-          hintText: text,
+          hintText: widget.text,
           hintStyle: Theme.of(context).textTheme.titleSmall!.copyWith(
-                color: color,
+                color: widget.color,
               ),
+          contentPadding: EdgeInsets.all(8.0),
+          errorText: widget.errorText,
           border: OutlineInputBorder(
             borderSide: BorderSide(width: 5.0),
             borderRadius: BorderRadius.circular(8.0),
           ),
+          fillColor: Theme.of(context).colorScheme.secondary.withOpacity(0.235),
         ),
+        onEditingComplete: () {
+          FocusScope.of(context).nextFocus();
+        },
+        validator: widget.validator,
+        onChanged: widget.onChanged,
       ),
     );
   }
