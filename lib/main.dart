de9c07d3295975885147367607cbc75fd3c26b9e
lantrannk <lantrannk@gmail.com>
2023-03-21 17:33:40 +0700

    remove device preview

--- lib/main.dart
+++ lib/main.dart
@@ -4,14 +4,9 @@ import 'package:salon_appointment/screens/calendar_screen.dart';
 import 'package:salon_appointment/screens/scaffold.dart';
 import 'package:salon_appointment/screens/splash_screen.dart';
 import 'theme/theme.dart';
-import 'package:device_preview/device_preview.dart';
 
 void main() {
   runApp(
-    // DevicePreview(
-    //   enabled: true,
-    //   builder: (context) => const MyApp(),
-    // ),
     const MyApp(),
   );
 }
