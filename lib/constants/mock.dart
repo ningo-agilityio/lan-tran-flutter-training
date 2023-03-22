a8cc7605805fc3e27829fc28a089a85b78df0456
lantrannk <lantrannk@gmail.com>
2023-03-22 14:28:53 +0700

    fetch data appointments

--- lib/constants/mock.dart
+++ lib/constants/mock.dart
@@ -1,14 +1,13 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:salon_appointment/models/appointment.dart';
 import 'package:salon_appointment/models/user.dart';
 
-// List<User> usersList = [];
+const String apiUrl = "https://63ab8e97fdc006ba60609b9b.mockapi.io/endpoint";
 
 class MockData {
   static Future<List<User>> getUsers() async {
-    const String usersApi =
-        "https://63ab8e97fdc006ba60609b9b.mockapi.io/endpoint/users";
-    final url = Uri.parse(usersApi);
+    final url = Uri.parse("$apiUrl/users");
 
     final response = await http.get(url);
     if (response.statusCode == 200) {
@@ -18,6 +17,20 @@ class MockData {
       return responseData;
     } else {
       throw Exception('Failed to load users.');
+    }
+  }
+
+  static Future<List<Appointment>> getAppointments() async {
+    final url = Uri.parse("$apiUrl/appointments");
+
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final responseData = (json.decode(response.body) as List)
+          .map((appointment) => Appointment.fromJson(appointment))
+          .toList();
+      return responseData;
+    } else {
+      throw Exception('Failed to load appointments.');
     }
   }
 }
