1db1eb787143a9f11d842ee6847c4f6f705cfe92
lantrannk <lantrannk@gmail.com>
2023-03-24 17:52:16 +0700

    fix bug linters

--- lib/constants/mock.dart
+++ lib/constants/mock.dart
@@ -3,16 +3,16 @@ import 'package:http/http.dart' as http;
 import 'package:salon_appointment/models/appointment.dart';
 import 'package:salon_appointment/models/user.dart';
 
-const String apiUrl = "https://63ab8e97fdc006ba60609b9b.mockapi.io/endpoint";
+const String apiUrl = 'https://63ab8e97fdc006ba60609b9b.mockapi.io/endpoint';
 
 class MockData {
   static Future<List<User>> getUsers() async {
-    final url = Uri.parse("$apiUrl/users");
+    final url = Uri.parse('$apiUrl/users');
 
     final response = await http.get(url);
     if (response.statusCode == 200) {
       final responseData = (json.decode(response.body) as List)
-          .map((user) => User.fromJson(user))
+          .map((user) => User.fromJson(user as Map<String, dynamic>))
           .toList();
       return responseData;
     } else {
@@ -26,7 +26,8 @@ class MockData {
     final response = await http.get(url);
     if (response.statusCode == 200) {
       final responseData = (json.decode(response.body) as List)
-          .map((appointment) => Appointment.fromJson(appointment))
+          .map((appointment) =>
+              Appointment.fromJson(appointment as Map<String, dynamic>))
           .toList();
       return responseData;
     } else {
--- lib/models/appointment.dart
+++ lib/models/appointment.dart
@@ -1,14 +1,6 @@
 import 'package:flutter/material.dart';
 
 class Appointment {
-  final String services;
-  final DateTime date;
-  final TimeOfDay startTime;
-  final TimeOfDay endTime;
-  final String userId;
-  final String description;
-  final bool isCompleted;
-
   Appointment({
     required this.userId,
     required this.date,
@@ -21,15 +13,23 @@ class Appointment {
 
   factory Appointment.fromJson(Map<dynamic, dynamic> appointment) {
     return Appointment(
-      userId: appointment['userId'],
+      userId: appointment['userId'] as String,
       date: DateTime.parse(appointment['date'].toString()),
       startTime: TimeOfDay.fromDateTime(
           DateTime.parse(appointment['startTime'].toString())),
       endTime: TimeOfDay.fromDateTime(
           DateTime.parse(appointment['endTime'].toString())),
-      services: appointment['services'],
-      description: appointment['description'],
+      services: appointment['services'] as String,
+      description: appointment['description'] as String,
       isCompleted: appointment['isCompleted'] as bool,
     );
   }
+
+  final String services;
+  final DateTime date;
+  final TimeOfDay startTime;
+  final TimeOfDay endTime;
+  final String userId;
+  final String description;
+  final bool isCompleted;
 }
