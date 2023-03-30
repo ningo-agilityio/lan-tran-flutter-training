ac5f085b35f8c0e2fb6448c3a0d74ec065d7d717
lantrannk <lantrannk@gmail.com>
2023-03-31 01:39:45 +0700

    create controller load appointments of date

---
+++ lib/controllers/appointment_controller.dart
@@ -0,0 +1,20 @@
+import 'package:collection/collection.dart';
+import 'package:intl/intl.dart';
+import 'package:salon_appointment/mock_apis/appointment_api.dart';
+import 'package:salon_appointment/models/appointment.dart';
+
+class AppointmentController {
+  Future<List<Appointment>> load(DateTime date) async {
+    final appointments = await AppointmentApi.getAppointments();
+    final dateMap = groupBy(appointments, (p0) => p0.date);
+    final dateStr = DateFormat.yMd().format(date);
+
+    for (final d in dateMap.keys) {
+      if (DateFormat.yMd().format(d) == dateStr) {
+        return dateMap[d]!;
+      }
+    }
+
+    return [];
+  }
+}
