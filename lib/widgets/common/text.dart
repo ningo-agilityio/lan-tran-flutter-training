e7e15acd981d0b914b2412c57a3f6eef4f182442
lantrannk <lantrannk@gmail.com>
2023-04-03 14:27:53 +0700

    define custom text

--- lib/widgets/common/text.dart
+++ lib/widgets/common/text.dart
@@ -1,26 +1,63 @@
 import 'package:flutter/material.dart';
 import 'package:salon_appointment/theme/theme.dart';
 
+class CustomText extends StatelessWidget {
+  const CustomText({required this.text, required this.style, super.key});
+
+  const factory CustomText.appBarTitle({
+    required String text,
+    required TextStyle style,
+  }) = _AppBarTitle;
+
+  final String text;
+  final TextStyle style;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: style,
+    );
+  }
+}
+
+class _AppBarTitle extends CustomText {
+  const _AppBarTitle({
+    required super.text,
+    required super.style,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: style.copyWith(
+        color: lightTheme.colorScheme.onSecondary,
+      ),
+    );
+  }
+}
+
 class SACustomText {
   static Text loginText = Text(
     'Login',
-    style: SATheme.lightTheme.textTheme.titleSmall!.copyWith(
-      color: SATheme.lightTheme.colorScheme.secondary,
+    style: lightTheme.textTheme.labelMedium!.copyWith(
+      color: lightTheme.colorScheme.secondary,
     ),
   );
 
   static Text forgetPasswordText = Text(
     'Forget password?',
-    style: SATheme.lightTheme.textTheme.bodyLarge!.copyWith(
-      color: SATheme.lightTheme.colorScheme.secondary.withOpacity(0.6429),
+    style: lightTheme.textTheme.bodySmall!.copyWith(
+      color: lightTheme.colorScheme.secondary.withOpacity(0.6429),
     ),
   );
 
   static Text logoText = Text(
     'avisit',
-    style: SATheme.lightTheme.textTheme.titleLarge!.copyWith(
-      fontSize: 40.0,
-      color: SATheme.lightTheme.colorScheme.secondary,
+    style: TextStyle(
+      fontSize: 40,
+      color: lightTheme.colorScheme.secondary,
     ),
   );
 }
