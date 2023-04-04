bd89171e3a1bad810708e551e72952cbc1a46007
lantrannk <lantrannk@gmail.com>
2023-04-04 16:07:33 +0700

    fix comments PR 28

--- lib/widgets/common/icons.dart
+++ lib/widgets/common/icons.dart
@@ -1,28 +1,27 @@
 import 'package:flutter/material.dart';
-import 'package:salon_appointment/theme/theme.dart';
 
 class CustomIcons {
-  static Icon appointmentScheduleIcon = Icon(
+  static Icon appointmentSchedule = Icon(
     Icons.schedule_outlined,
     size: 24,
-    color: lightTheme.colorScheme.tertiary,
+    color: themeData.colorScheme.tertiary,
   );
 
-  static Icon breakTimeIcon = Icon(
+  static Icon breakTime = Icon(
     Icons.schedule_outlined,
     size: 24,
-    color: lightTheme.colorScheme.onSecondaryContainer,
+    color: themeData.colorScheme.onSecondaryContainer,
   );
 
-  static Icon calendarScheduleIcon = Icon(
+  static Icon calendarSchedule = Icon(
     Icons.schedule_outlined,
     size: 24,
-    color: lightTheme.colorScheme.secondary,
+    color: themeData.colorScheme.secondary,
   );
 
-  static Icon addButtonIcon = Icon(
+  static Icon addButton = Icon(
     Icons.add,
     size: 30,
-    color: lightTheme.colorScheme.secondary,
+    color: themeData.colorScheme.secondary,
   );
 }
