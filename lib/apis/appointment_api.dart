0c2cc0a99eb4a76129e1227749cf86571c8d96dc
lantrannk <lantrannk@gmail.com>
2023-04-05 12:12:04 +0700

    handle errors for hitting appointment api

---
+++ lib/apis/appointment_api.dart
@@ -0,0 +1,48 @@
+import 'dart:convert';
+import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
+import 'package:salon_appointment/models/appointment.dart';
+
+const String apiUrl = 'https://63ab8e97fdc006ba60609b9b.mockapi.io/endpoint';
+
+class AppointmentApi {
+  static Future<List<Appointment>> getAppointments() async {
+    final url = Uri.parse('$apiUrl/appointments');
+    late List<Appointment> responseData;
+
+    final response = await http.get(url);
+
+    try {
+      if (response.statusCode == 200) {
+        responseData = (json.decode(response.body) as List)
+            .map(
+              (appointment) =>
+                  Appointment.fromJson(appointment as Map<dynamic, dynamic>),
+            )
+            .toList();
+      } else {
+        throw Exception('Failed to load appointments.');
+      }
+    } catch (e) {
+      if (kDebugMode) {
+        print(e);
+      }
+    }
+
+    return responseData;
+  }
+
+  // static Future<Appointment> addAppointment() async {
+  //   final response = await http.post(
+  //     Uri.parse('https://jsonplaceholder.typicode.com/albums'),
+
+  //   );
+
+  //   if (response.statusCode == 201) {
+  //     return Appointment.fromJson(
+  //         jsonDecode(response.body) as Map<dynamic, dynamic>);
+  //   } else {
+  //     throw Exception('Failed to create appointment.');
+  //   }
+  // }
+}
