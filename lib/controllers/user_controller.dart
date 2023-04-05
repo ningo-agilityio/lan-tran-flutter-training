9aaa25d4afb12963e5c4afae497f261915fdf0da
lantrannk <lantrannk@gmail.com>
2023-04-05 17:28:49 +0700

    get logged in user

---
+++ lib/controllers/user_controller.dart
@@ -0,0 +1,20 @@
+import '../apis/user_api.dart';
+import '../models/user.dart';
+
+class UserController {
+  Future<User?> getUser(
+    String? phoneNumber,
+    String? password,
+  ) async {
+    final users = await UserApi.getUsers();
+    if (users
+        .any((e) => e.phoneNumber == phoneNumber && e.password == password)) {
+      final user = users
+          .where((e) => e.phoneNumber == phoneNumber && e.password == password)
+          .first;
+      return user;
+    }
+
+    return null;
+  }
+}
