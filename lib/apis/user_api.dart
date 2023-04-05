da0217cb153d94a9a1a6cfab5341ec9bd3260955
lantrannk <lantrannk@gmail.com>
2023-04-05 12:12:28 +0700

    handle errors for hitting user api

---
+++ lib/apis/user_api.dart
@@ -0,0 +1,29 @@
+import 'dart:convert';
+import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
+import 'package:salon_appointment/models/user.dart';
+
+const String apiUrl = 'https://63ab8e97fdc006ba60609b9b.mockapi.io/endpoint';
+
+class UserApi {
+  static Future<List<User>> getUsers() async {
+    final url = Uri.parse('$apiUrl/users');
+    late List<User> responseData;
+
+    final response = await http.get(url);
+    try {
+      if (response.statusCode == 200) {
+        responseData = (json.decode(response.body) as List)
+            .map((user) => User.fromJson(user as Map<String, dynamic>))
+            .toList();
+      } else {
+        throw Exception('Failed to load users.');
+      }
+    } catch (e) {
+      if (kDebugMode) {
+        print(e);
+      }
+    }
+    return responseData;
+  }
+}
