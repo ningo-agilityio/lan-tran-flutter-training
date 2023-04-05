d8b65c1c3ec84cfd2e3ad70d63e08f55af4dfe96
lantrannk <lantrannk@gmail.com>
2023-04-05 17:22:32 +0700

    refactor widget time

--- lib/widgets/time.dart
+++ lib/widgets/time.dart
@@ -1,43 +1,36 @@
 import 'package:flutter/material.dart';
-import 'package:salon_appointment/theme/theme.dart';
+import 'package:salon_appointment/widgets/common/icons.dart';
 
 class Time extends StatelessWidget {
+  const Time({
+    required this.startTime,
+    required this.endTime,
+    super.key,
+  });
+
   final TimeOfDay startTime;
   final TimeOfDay endTime;
-  Time({super.key, required this.startTime, required this.endTime});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        children: [
-          Row(
-            children: [
-              Icon(
-                Icons.schedule_sharp,
-                color: Color.fromARGB(255, 255, 170, 0),
-                size: 20,
+    return Column(
+      children: [
+        Row(
+          children: [
+            CustomIcons.appointmentSchedule,
+            const SizedBox(
+              width: 10,
+            ),
+            Text(
+              '${startTime.format(context)}-${endTime.format(context)}',
+              style: const TextStyle(
+                fontSize: 14,
+                color: Color(0xFF0C122A),
               ),
-              SizedBox(
-                width: 10.0,
-              ),
-              Text(
-                '${startTime.format(context)}-${endTime.format(context)}',
-                style: TextStyle(
-                  fontSize: 14,
-                  color: Color(0xFF0C122A),
-                ),
-              )
-            ],
-          ),
-          // Text(
-          //   'Beauty Salon',
-          //   style: SATheme.lightTheme.textTheme.bodySmall!.copyWith(
-          //     color: Color(0xFFA4A8B2),
-          //   ),
-          // ),
-        ],
-      ),
+            )
+          ],
+        ),
+      ],
     );
   }
 }
