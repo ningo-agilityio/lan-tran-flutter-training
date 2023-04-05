33f8e5c22a2824eaa4cd11149db4f237e8306d70
lantrannk <lantrannk@gmail.com>
2023-04-05 17:15:28 +0700

    refactor common widget icons

--- lib/widgets/common/icons.dart
+++ lib/widgets/common/icons.dart
@@ -2,25 +2,25 @@ import 'package:flutter/material.dart';
 import 'package:salon_appointment/theme/theme.dart';
 
 class CustomIcons {
-  static Icon appointmentScheduleIcon = Icon(
+  static Icon appointmentSchedule = Icon(
     Icons.schedule_outlined,
-    size: 24,
+    size: 20,
     color: themeData.colorScheme.tertiary,
   );
 
-  static Icon breakTimeIcon = Icon(
+  static Icon breakTime = Icon(
     Icons.schedule_outlined,
-    size: 24,
+    size: 20,
     color: themeData.colorScheme.onSecondaryContainer,
   );
 
-  static Icon calendarScheduleIcon = Icon(
+  static Icon calendarSchedule = Icon(
     Icons.schedule_outlined,
-    size: 24,
+    size: 20,
     color: themeData.colorScheme.secondary,
   );
 
-  static Icon addButtonIcon = Icon(
+  static Icon addButton = Icon(
     Icons.add,
     size: 30,
     color: themeData.colorScheme.secondary,
