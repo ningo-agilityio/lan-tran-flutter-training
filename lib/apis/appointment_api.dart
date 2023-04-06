9913fbcc9958ebfa772afceb50e34eb027ab72ea
lantrannk <lantrannk@gmail.com>
2023-04-07 04:17:46 +0700

    create api add appointment

--- lib/apis/appointment_api.dart
+++ lib/apis/appointment_api.dart
@@ -8,7 +8,7 @@ import 'constants.dart';
 class AppointmentApi {
   static Future<List<Appointment>> getAppointments(String userId) async {
     final url = Uri.parse('$apiUrl/appointments?userId=$userId');
-    late List<Appointment> responseData;
+    late List<Appointment> responseData = [];
 
     final response = await http.get(url);
 
@@ -30,5 +30,38 @@ class AppointmentApi {
     }
 
     return responseData;
+  }
+
+  static Future<void> addAppointment(Appointment appointment) async {
+    final url = Uri.parse('$apiUrl/appointments');
+
+    final headers = {'Content-Type': 'application/json'};
+
+    // Map<dynamic, dynamic> match = {
+    //   'date': '2023-04-06',
+    //   'startTime': '2023-04-06T15:58:14.719Z',
+    //   'endTime': '2023-04-06T17:21:45.867Z',
+    //   'userId': '1',
+    //   'services': 'services 1',
+    //   'description':
+    //       'There is a distinction between a beauty salon and a hair salon and although many small businesses do offer both sets of treatments; beauty salons provide extended services related to skin health, facial aesthetic, foot care, nail manicures, aromatherapy — even meditation, oxygen therapy, mud baths and many other services.',
+    //   'isCompleted': false,
+    // };
+
+    final map = appointment.toJson();
+
+    final body = json.encode(map);
+
+    try {
+      await http.post(url, body: body, headers: headers).then((response) {
+        if (kDebugMode) {
+          print(response.body);
+        }
+      });
+    } catch (e) {
+      if (kDebugMode) {
+        print(e.toString());
+      }
+    }
   }
 }
