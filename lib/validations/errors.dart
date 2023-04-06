14554fde5b443635bce9a4a756517c72edd90dfa
lantrannk <lantrannk@gmail.com>
2023-04-06 09:50:47 +0700

    define constants: errors

---
+++ lib/validations/errors.dart
@@ -0,0 +1,6 @@
+class Errors {
+  static const String INVALID_ACCOUNT = 'Phone number or Password is invalid.';
+
+  static const String INCORRECT_ACCOUNT =
+      'Phone number or Password is incorrect.';
+}
