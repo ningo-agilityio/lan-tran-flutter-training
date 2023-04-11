59984e5d1ebb7d3dc7c93cf156d9645db62f6c50
lantrannk <lantrannk@gmail.com>
2023-04-11 16:20:05 +0700

    generate i10n

---
+++ lib/generated/intl/messages_en.dart
@@ -0,0 +1,46 @@
+// DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
+// This is a library that provides messages for a en locale. All the
+// messages from the main program should be duplicated here with the same
+// function name.
+
+// Ignore issues from commonly used lints in this file.
+// ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
+// ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
+// ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
+// ignore_for_file:unused_import, file_names, avoid_escaping_inner_quotes
+// ignore_for_file:unnecessary_string_interpolations, unnecessary_string_escapes
+
+import 'package:intl/intl.dart';
+import 'package:intl/message_lookup_by_library.dart';
+
+final messages = new MessageLookup();
+
+typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
+
+class MessageLookup extends MessageLookupByLibrary {
+  String get localeName => 'en';
+
+  final messages = _notInlinedMessages(_notInlinedMessages);
+  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
+        "addFail": MessageLookupByLibrary.simpleMessage(
+            "Failed to create appointment"),
+        "addSuccess": MessageLookupByLibrary.simpleMessage("Appointment added"),
+        "appointmentAppBarTitle":
+            MessageLookupByLibrary.simpleMessage("Appointments"),
+        "calendarAppBarTitle": MessageLookupByLibrary.simpleMessage("Calendar"),
+        "createAppointmentButton":
+            MessageLookupByLibrary.simpleMessage("Create New Appointment"),
+        "description": MessageLookupByLibrary.simpleMessage("Description"),
+        "emptyAppointments":
+            MessageLookupByLibrary.simpleMessage("There are no appointments."),
+        "forgotPassword":
+            MessageLookupByLibrary.simpleMessage("Forgot Password?"),
+        "loginButton": MessageLookupByLibrary.simpleMessage("Login"),
+        "logo": MessageLookupByLibrary.simpleMessage("avisit"),
+        "logoText": MessageLookupByLibrary.simpleMessage("av"),
+        "newAppointmentAppBarTitle":
+            MessageLookupByLibrary.simpleMessage("New Appointment"),
+        "password": MessageLookupByLibrary.simpleMessage("Password"),
+        "phoneNumber": MessageLookupByLibrary.simpleMessage("Phone Number")
+      };
+}
---
+++ lib/generated/l10n.dart
@@ -0,0 +1,218 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'intl/messages_all.dart';
+
+// **************************************************************************
+// Generator: Flutter Intl IDE plugin
+// Made by Localizely
+// **************************************************************************
+
+// ignore_for_file: non_constant_identifier_names, lines_longer_than_80_chars
+// ignore_for_file: join_return_with_assignment, prefer_final_in_for_each
+// ignore_for_file: avoid_redundant_argument_values, avoid_escaping_inner_quotes
+
+class S {
+  S();
+
+  static S? _current;
+
+  static S get current {
+    assert(_current != null,
+        'No instance of S was loaded. Try to initialize the S delegate before accessing S.current.');
+    return _current!;
+  }
+
+  static const AppLocalizationDelegate delegate = AppLocalizationDelegate();
+
+  static Future<S> load(Locale locale) {
+    final name = (locale.countryCode?.isEmpty ?? false)
+        ? locale.languageCode
+        : locale.toString();
+    final localeName = Intl.canonicalizedLocale(name);
+    return initializeMessages(localeName).then((_) {
+      Intl.defaultLocale = localeName;
+      final instance = S();
+      S._current = instance;
+
+      return instance;
+    });
+  }
+
+  static S of(BuildContext context) {
+    final instance = S.maybeOf(context);
+    assert(instance != null,
+        'No instance of S present in the widget tree. Did you add S.delegate in localizationsDelegates?');
+    return instance!;
+  }
+
+  static S? maybeOf(BuildContext context) {
+    return Localizations.of<S>(context, S);
+  }
+
+  /// `avisit`
+  String get logo {
+    return Intl.message(
+      'avisit',
+      name: 'logo',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `av`
+  String get logoText {
+    return Intl.message(
+      'av',
+      name: 'logoText',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Phone Number`
+  String get phoneNumber {
+    return Intl.message(
+      'Phone Number',
+      name: 'phoneNumber',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Password`
+  String get password {
+    return Intl.message(
+      'Password',
+      name: 'password',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Forgot Password?`
+  String get forgotPassword {
+    return Intl.message(
+      'Forgot Password?',
+      name: 'forgotPassword',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Login`
+  String get loginButton {
+    return Intl.message(
+      'Login',
+      name: 'loginButton',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Calendar`
+  String get calendarAppBarTitle {
+    return Intl.message(
+      'Calendar',
+      name: 'calendarAppBarTitle',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Appointments`
+  String get appointmentAppBarTitle {
+    return Intl.message(
+      'Appointments',
+      name: 'appointmentAppBarTitle',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `New Appointment`
+  String get newAppointmentAppBarTitle {
+    return Intl.message(
+      'New Appointment',
+      name: 'newAppointmentAppBarTitle',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `There are no appointments.`
+  String get emptyAppointments {
+    return Intl.message(
+      'There are no appointments.',
+      name: 'emptyAppointments',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Description`
+  String get description {
+    return Intl.message(
+      'Description',
+      name: 'description',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Create New Appointment`
+  String get createAppointmentButton {
+    return Intl.message(
+      'Create New Appointment',
+      name: 'createAppointmentButton',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Appointment added`
+  String get addSuccess {
+    return Intl.message(
+      'Appointment added',
+      name: 'addSuccess',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Failed to create appointment`
+  String get addFail {
+    return Intl.message(
+      'Failed to create appointment',
+      name: 'addFail',
+      desc: '',
+      args: [],
+    );
+  }
+}
+
+class AppLocalizationDelegate extends LocalizationsDelegate<S> {
+  const AppLocalizationDelegate();
+
+  List<Locale> get supportedLocales {
+    return const <Locale>[
+      Locale.fromSubtags(languageCode: 'en'),
+    ];
+  }
+
+  @override
+  bool isSupported(Locale locale) => _isSupported(locale);
+  @override
+  Future<S> load(Locale locale) => S.load(locale);
+  @override
+  bool shouldReload(AppLocalizationDelegate old) => false;
+
+  bool _isSupported(Locale locale) {
+    for (var supportedLocale in supportedLocales) {
+      if (supportedLocale.languageCode == locale.languageCode) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
