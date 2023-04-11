ed4df7e0951e669a958209f302f4dd0487b896d5
lantrannk <lantrannk@gmail.com>
2023-04-11 16:18:01 +0700

    update layouts

---
+++ lib/core/layouts/common_layout.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:salon_appointment/constants.dart';
+import 'package:salon_appointment/core/theme/theme.dart';
+
+class CommonLayout extends StatelessWidget {
+  const CommonLayout({
+    required this.child,
+    this.bgColor,
+    this.bgImage,
+    super.key,
+  });
+
+  final Widget child;
+  final Color? bgColor;
+  final ImageProvider<Object>? bgImage;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: bgImage ?? const AssetImage(bgImagePath),
+            colorFilter: ColorFilter.mode(
+              bgColor ?? themeData.colorScheme.primary.withOpacity(0.5),
+              BlendMode.multiply,
+            ),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: child,
+      ),
+    );
+  }
+}
---
+++ lib/core/layouts/main_layout.dart
@@ -0,0 +1,119 @@
+import 'package:flutter/material.dart';
+
+import '../theme/theme.dart';
+import '../widgets/buttons.dart';
+import '../widgets/icons.dart';
+import '../widgets/text.dart';
+
+class MainLayout extends StatefulWidget {
+  const MainLayout({
+    required this.title,
+    required this.child,
+    required this.currentIndex,
+    super.key,
+  });
+
+  final String title;
+  final Widget child;
+  final int currentIndex;
+
+  @override
+  State<MainLayout> createState() => _MainLayoutState();
+}
+
+class _MainLayoutState extends State<MainLayout> {
+  final FloatingActionButtonLocation _fabLocation =
+      FloatingActionButtonLocation.centerDocked;
+
+  late int _currentIndex;
+
+  @override
+  void initState() {
+    _currentIndex = widget.currentIndex;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: CustomText.appBarTitle(
+          text: widget.title,
+          style: themeData.textTheme.titleLarge!,
+        ),
+        automaticallyImplyLeading: false,
+      ),
+      body: widget.child,
+      floatingActionButton: SAButton.floating(
+        child: CustomIcons.add,
+        onPressed: () => Navigator.of(context).pushNamed('/add'),
+      ),
+      floatingActionButtonLocation: _fabLocation,
+      bottomNavigationBar: CusTomBottomAppBar(
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+            switch (_currentIndex) {
+              case 0:
+                // Appointment Screen
+                Navigator.of(context).pushNamed('/appointment');
+                break;
+              case 1:
+                // Calendar Screen
+                Navigator.of(context).pushNamed('/calendar');
+                break;
+            }
+          });
+        },
+      ),
+    );
+  }
+}
+
+class CusTomBottomAppBar extends StatelessWidget {
+  const CusTomBottomAppBar({
+    required this.currentIndex,
+    required this.onTap,
+    super.key,
+  });
+
+  final int currentIndex;
+  final Function(int) onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomAppBar(
+      shape: const CircularNotchedRectangle(),
+      clipBehavior: Clip.antiAlias,
+      child: BottomNavigationBar(
+        backgroundColor: themeData.colorScheme.onPrimary,
+        currentIndex: currentIndex,
+        onTap: onTap,
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: themeData.colorScheme.primary,
+        unselectedItemColor: themeData.colorScheme.onTertiary,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.check),
+            label: 'Appointments',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.schedule),
+            label: 'Calendar',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.notifications),
+            label: 'Notifications',
+          ),
+        ],
+      ),
+    );
+  }
+}
