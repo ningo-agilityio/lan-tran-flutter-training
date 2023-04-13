6615f14996e5055ab7d1c8c2e9465b08b08ec3a2
lantrannk <lantrannk@gmail.com>
2023-04-13 11:58:33 +0700

    refactor code

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
 import 'core/theme/theme.dart';
-import 'features/appointments/screens/appointments_screen.dart';
-import 'features/appointments/screens/calendar_screen.dart';
-import 'features/appointments/screens/new_appointment_screen.dart';
 import 'features/auth/screens/splash_screen.dart';
 import 'generated/l10n.dart';
 
@@ -23,11 +20,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: themeData,
-      routes: {
-        '/calendar': (context) => const CalendarScreen(),
-        '/appointment': (context) => const AppointmentScreen(),
-        '/add': (context) => const NewAppointmentScreen(),
-      },
       localizationsDelegates: const [
         S.delegate,
         GlobalMaterialLocalizations.delegate,
