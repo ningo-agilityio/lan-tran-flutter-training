6e27bf84e6c64fb5ebfae0550ce7d28dea56c89d
lantrannk <lantrannk@gmail.com>
2023-04-15 02:53:24 +0700

    get user id from local storage to fetch api

--- lib/features/appointments/repository/appointment_repository.dart
+++ lib/features/appointments/repository/appointment_repository.dart
@@ -2,10 +2,13 @@ import 'package:collection/collection.dart';
 import 'package:intl/intl.dart';
 import 'package:salon_appointment/features/appointments/api/appointment_api.dart';
 import 'package:salon_appointment/features/appointments/model/appointment.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AppointmentRepository {
-  Future<List<Appointment>> load(DateTime date, String id) async {
-    final appointments = await AppointmentApi.getAppointments(id);
+  Future<List<Appointment>> load(DateTime date) async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    final String? id = prefs.getString('id');
+    final appointments = await AppointmentApi.getAppointments(id!);
     final dateMap = groupBy(appointments, (p0) => p0.date);
     final dateStr = DateFormat.yMd().format(date);
 
