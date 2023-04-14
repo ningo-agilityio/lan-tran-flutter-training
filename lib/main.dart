ecc1eb605f9389baa141d4d4ff5f13ddfa8d0ba3
lantrannk <lantrannk@gmail.com>
2023-04-14 09:46:29 +0700

    rename title app

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Salon Appointment',
       theme: themeData,
       localizationsDelegates: const [
         S.delegate,
