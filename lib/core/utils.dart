bd6cbeb9508fe93f836877828888b6fea22ccf83
lantrannk <lantrannk@gmail.com>
2023-04-16 01:35:11 +0700

    fix util get data from storage

--- lib/core/utils.dart
+++ lib/core/utils.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:shared_preferences/shared_preferences.dart';
 
 /// Returns a list of [DateTime] objects from [first] to [last], inclusive.
@@ -13,27 +15,8 @@ final kToday = DateTime.now();
 final kFirstDay = DateTime(kToday.year - 10, kToday.month, kToday.day);
 final kLastDay = DateTime(kToday.year + 10, kToday.month, kToday.day);
 
-Future<String> getUserName() async {
+Future<Map<String, dynamic>> getUser() async {
   final SharedPreferences prefs = await SharedPreferences.getInstance();
-  return prefs.getString('name')!;
-}
-
-Future<String> getUserPhoneNumber() async {
-  final SharedPreferences prefs = await SharedPreferences.getInstance();
-  return prefs.getString('phoneNumber')!;
-}
-
-Future<String> getUserAvatar() async {
-  final SharedPreferences prefs = await SharedPreferences.getInstance();
-  return prefs.getString('avatar')!;
-}
-
-Future<String> getUserPassword() async {
-  final SharedPreferences prefs = await SharedPreferences.getInstance();
-  return prefs.getString('password')!;
-}
-
-Future<String?> getUserId() async {
-  final SharedPreferences prefs = await SharedPreferences.getInstance();
-  return prefs.getString('id');
+  final String userStr = prefs.getString('user') ?? '{}';
+  return jsonDecode(userStr);
 }
