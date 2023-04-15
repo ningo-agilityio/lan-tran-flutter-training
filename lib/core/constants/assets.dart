e2d8523cb345d2e92514dec0983e70376ef6c8a5
lantrannk <lantrannk@gmail.com>
2023-04-15 12:03:44 +0700

    define icons contants

--- lib/core/constants/assets.dart
+++ lib/core/constants/assets.dart
@@ -2,4 +2,20 @@ import 'package:flutter/material.dart';
 
 class Assets {
   static const AssetImage bgImage = AssetImage('assets/background.png');
+
+  static const IconData scheduleIcon = Icons.schedule_outlined;
+
+  static const IconData addIcon = Icons.add;
+
+  static const IconData closeIcon = Icons.close;
+
+  static const IconData checkIcon = Icons.check;
+
+  static const IconData personIcon = Icons.person;
+
+  static const IconData notificationsIcon = Icons.notifications;
+
+  static const IconData calendarIcon = Icons.calendar_month;
+
+  static const IconData dropdownIcon = Icons.arrow_drop_down;
 }
