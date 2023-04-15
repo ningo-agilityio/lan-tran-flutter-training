b2670e0799ec1ee76ac3f4a80c363a57a8ec9861
lantrannk <lantrannk@gmail.com>
2023-04-16 01:37:51 +0700

    update load()

--- lib/features/appointments/repository/appointment_repository.dart
+++ lib/features/appointments/repository/appointment_repository.dart
@@ -1,14 +1,14 @@
 import 'package:collection/collection.dart';
 import 'package:intl/intl.dart';
+import 'package:salon_appointment/core/utils.dart';
 import 'package:salon_appointment/features/appointments/api/appointment_api.dart';
 import 'package:salon_appointment/features/appointments/model/appointment.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 class AppointmentRepository {
   Future<List<Appointment>> load(DateTime date) async {
-    final SharedPreferences prefs = await SharedPreferences.getInstance();
-    final String? id = prefs.getString('id');
-    final appointments = await AppointmentApi.getAppointments(id!);
+    final Map<String, dynamic> userJson = await getUser();
+
+    final appointments = await AppointmentApi.getAppointments(userJson['id']);
     final dateMap = groupBy(appointments, (p0) => p0.date);
     final dateStr = DateFormat.yMd().format(date);
 
