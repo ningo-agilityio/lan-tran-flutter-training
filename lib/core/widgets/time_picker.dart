86b68e4d0a5b14e85cb9898088f22129b44ba7d3
lantrannk <lantrannk@gmail.com>
2023-04-17 00:40:24 +0700

    format time

--- lib/core/widgets/time_picker.dart
+++ lib/core/widgets/time_picker.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:salon_appointment/core/utils.dart';
 import 'package:salon_appointment/core/widgets/text.dart';
 
 class TimePicker extends StatelessWidget {
@@ -24,9 +25,7 @@ class TimePicker extends StatelessWidget {
         OutlinedButton(
           onPressed: onStartTimePressed,
           child: SAText.timePicker(
-            text: (startTime.minute < 10)
-                ? '${startTime.hour}:${startTime.minute.toString().padLeft(2, '0')}'
-                : '${startTime.hour}:${startTime.minute}',
+            text: formatTime(startTime),
           ),
         ),
         const Padding(
@@ -36,9 +35,7 @@ class TimePicker extends StatelessWidget {
         OutlinedButton(
           onPressed: onEndTimePressed,
           child: SAText.timePicker(
-            text: (endTime.minute < 10)
-                ? '${endTime.hour}:${endTime.minute.toString().padLeft(2, '0')}'
-                : '${endTime.hour}:${endTime.minute}',
+            text: formatTime(endTime),
           ),
         ),
       ],
