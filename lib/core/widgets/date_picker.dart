a3afe716190d137ecc8ab59e4b613d1a059c2778
lantrannk <lantrannk@gmail.com>
2023-04-16 23:02:27 +0700

    define date picker as common widget

---
+++ lib/core/widgets/date_picker.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class DatePicker extends StatelessWidget {
+  const DatePicker({
+    required this.dateTime,
+    required this.onPressed,
+    super.key,
+  });
+
+  final VoidCallback onPressed;
+  final DateTime dateTime;
+
+  @override
+  Widget build(BuildContext context) {
+    final DateFormat dateFormat = DateFormat('MM/dd/yyyy');
+
+    return Padding(
+      padding: const EdgeInsets.only(left: 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Icon(
+            Icons.schedule,
+            size: 24,
+            color: Theme.of(context).colorScheme.secondaryContainer,
+          ),
+          Text(
+            dateFormat.format(dateTime),
+            style: Theme.of(context).textTheme.labelLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.secondaryContainer,
+                  fontWeight: FontWeight.w400,
+                ),
+          ),
+          Align(
+            alignment: Alignment.centerRight,
+            child: IconButton(
+              onPressed: onPressed,
+              icon: Icon(
+                Icons.calendar_month,
+                size: 24,
+                color: Theme.of(context).colorScheme.secondaryContainer,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
