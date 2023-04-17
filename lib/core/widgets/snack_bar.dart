78adf56d5a40dde194565bb92322c3326d6fa647
lantrannk <lantrannk@gmail.com>
2023-04-17 11:40:02 +0700

    fix background color

--- lib/core/widgets/snack_bar.dart
+++ lib/core/widgets/snack_bar.dart
@@ -4,12 +4,14 @@ class SASnackBar {
   static void show({
     required BuildContext context,
     required String message,
+    required bool isSuccess,
   }) {
+    final ColorScheme colorScheme = Theme.of(context).colorScheme;
     final snackBar = SnackBar(
       elevation: 5,
       behavior: SnackBarBehavior.floating,
       duration: const Duration(seconds: 3),
-      backgroundColor: Theme.of(context).colorScheme.primary,
+      backgroundColor: isSuccess ? colorScheme.primary : colorScheme.error,
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(10),
       ),
