b1562f2fa67188a1b0cce48a327950d2601dee2f
lantrannk <lantrannk@gmail.com>
2023-04-17 11:57:25 +0700

    add comments for functions

--- lib/core/utils.dart
+++ lib/core/utils.dart
@@ -13,10 +13,12 @@ List<DateTime> daysInRange(DateTime first, DateTime last) {
   );
 }
 
+/// Returns a [DateTime] added 30 minutes from [time]
 DateTime autoAddHalfHour(DateTime time) {
   return time.add(const Duration(minutes: 30));
 }
 
+/// returns a [DateTime] with date of [date] and time of [time]
 DateTime setDateTime(DateTime date, TimeOfDay time) {
   return DateTime(
     date.year,
@@ -27,11 +29,13 @@ DateTime setDateTime(DateTime date, TimeOfDay time) {
   );
 }
 
+/// Returns a [String] of [DateTime] formatted from [time]
 String formatTime(DateTime time) {
   final DateFormat timeFormat = DateFormat('HH:mm');
   return timeFormat.format(time).toString();
 }
 
+/// Returns [bool] that [start] or [end] conflict the break time or not
 bool isBreakTime(DateTime start, DateTime end) {
   final DateTime beforeTime =
       DateTime(start.year, start.month, start.day, 12, 0);
@@ -45,6 +49,7 @@ bool isBreakTime(DateTime start, DateTime end) {
   return false;
 }
 
+/// Returns [bool] that [start] or [end] conflict the close time or not
 bool isClosedTime(DateTime start, DateTime end) {
   final DateTime beforeTime =
       DateTime(start.year, start.month, start.day, 22, 0);
@@ -63,6 +68,7 @@ final kToday = DateTime.now();
 final kFirstDay = DateTime(kToday.year - 10, kToday.month, kToday.day);
 final kLastDay = DateTime(kToday.year + 10, kToday.month, kToday.day);
 
+/// Returns a [Map] of a user from storage
 Future<Map<String, dynamic>> getUser() async {
   final SharedPreferences prefs = await SharedPreferences.getInstance();
   final String userStr = prefs.getString('user') ?? '{}';
