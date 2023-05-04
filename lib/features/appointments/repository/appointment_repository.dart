93d8b3fed47277e345fd39f9a98099109dd2b1fe
lantrannk <lantrannk@gmail.com>
2023-05-04 09:39:44 +0700

    change groupBy to list.where

--- lib/features/appointments/repository/appointment_repository.dart
+++ lib/features/appointments/repository/appointment_repository.dart
@@ -1,4 +1,3 @@
-import 'package:collection/collection.dart';
 import 'package:intl/intl.dart';
 import 'package:salon_appointment/core/utils.dart';
 import 'package:salon_appointment/features/appointments/api/appointment_api.dart';
@@ -9,18 +8,12 @@ class AppointmentRepository {
     final Map<String, dynamic> userJson = await getUser();
 
     final appointments = await AppointmentApi.getAppointments(userJson['id']);
-    final dateMap = groupBy(
-      appointments,
-      (e) => DateFormat.yMd().format(e.date),
-    );
     final dateStr = DateFormat.yMd().format(date);
 
-    for (final d in dateMap.keys) {
-      if (d == dateStr) {
-        return dateMap[d]!;
-      }
-    }
+    final List<Appointment> appointmentsOfDate = appointments
+        .where((e) => DateFormat.yMd().format(e.date) == dateStr)
+        .toList();
 
-    return [];
+    return appointmentsOfDate;
   }
 }
