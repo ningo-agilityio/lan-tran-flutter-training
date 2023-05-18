62de8508cf09b4416b3592bc425302dc65951b9d
lantrannk <lantrannk@gmail.com>
2023-05-18 09:33:41 +0700

    bloc for fetch appointment

---
+++ lib/features/appointments/bloc/appointment_bloc.dart
@@ -0,0 +1,35 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:salon_appointment/core/storage/appointment_storage.dart';
+import 'package:salon_appointment/core/storage/user_storage.dart';
+import 'package:salon_appointment/features/appointments/api/appointment_api.dart';
+
+import '../model/appointment.dart';
+import 'appointment_event.dart';
+import 'appointment_state.dart';
+
+class AppointmentBloc extends Bloc<AppointmentEvent, AppointmentState> {
+  AppointmentBloc() : super(AppointmentInitial()) {
+    on<AppointmenFetch>((event, emit) => _getAppointmentList(emit));
+  }
+
+  Future<void> _getAppointmentList(Emitter emit) async {
+    try {
+      emit(AppointmentLoading());
+      final Map<String, dynamic> user = await UserStorage.getUser();
+      List<Appointment> appointments = [];
+      if (user['isAdmin']) {
+        appointments = await AppointmentStorage.getAppointments();
+        emit(
+          AppointmentLoadSuccess(appointments: appointments),
+        );
+      } else {
+        appointments = await AppointmentApi.getAppointmentsOfUser(user['id']);
+        emit(
+          AppointmentLoadSuccess(appointments: appointments),
+        );
+      }
+    } on Exception catch (e) {
+      emit(AppointmentLoadError(error: e.toString()));
+    }
+  }
+}
