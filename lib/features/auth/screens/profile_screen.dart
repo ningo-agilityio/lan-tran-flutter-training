46f6d9fd1efb96e6d58869f1726951d8fff6ebac
lantrannk <lantrannk@gmail.com>
2023-05-18 08:46:37 +0700

    apply bloc for logout

--- lib/features/auth/screens/profile_screen.dart
+++ lib/features/auth/screens/profile_screen.dart
@@ -1,8 +1,10 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:salon_appointment/core/layouts/main_layout.dart';
 import 'package:salon_appointment/core/widgets/text.dart';
+import 'package:salon_appointment/features/auth/bloc/auth_bloc.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import '../../../core/generated/l10n.dart';
@@ -27,113 +29,118 @@ class _ProfileScreenState extends State<ProfileScreen> {
     late Map<String, dynamic> user;
     final i10n = S.of(context);
 
-    return FutureBuilder(
-        future: UserStorage.getUser(),
-        builder: (_, snapshot) {
-          if (snapshot.hasData) {
-            user = snapshot.data!;
+    return BlocProvider(
+      create: (context) => AuthBloc(),
+      child: FutureBuilder(
+          future: UserStorage.getUser(),
+          builder: (_, snapshot) {
+            if (snapshot.hasData) {
+              user = snapshot.data!;
 
-            return MainLayout(
-              title: i10n.profileAppBarTitle,
-              currentIndex: 2,
-              child: Container(
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    colors: [
-                      colorScheme.primary,
-                      colorScheme.onSurface,
-                    ],
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter,
+              return MainLayout(
+                title: i10n.profileAppBarTitle,
+                currentIndex: 2,
+                child: Container(
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [
+                        colorScheme.primary,
+                        colorScheme.onSurface,
+                      ],
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                    ),
                   ),
-                ),
-                child: Center(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      const SizedBox(
-                        height: 100,
-                      ),
-                      Container(
-                        width: 150,
-                        height: 150,
-                        decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          color: colorScheme.onPrimary,
-                          image: DecorationImage(
-                            fit: BoxFit.fill,
-                            image: NetworkImage(user['avatar']),
-                          ),
+                  child: Center(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        const SizedBox(
+                          height: 100,
                         ),
-                      ),
-                      const SizedBox(
-                        height: 30,
-                      ),
-                      SAText(
-                        text: user['name'],
-                        style: textTheme.displaySmall!.copyWith(
-                          color: colorScheme.onPrimary,
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 200,
-                      ),
-                      Container(
-                        decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          color: Theme.of(context)
-                              .colorScheme
-                              .primary
-                              .withOpacity(0.1),
-                        ),
-                        child: SAButton.icon(
-                          child: Icon(
-                            Icons.logout,
+                        Container(
+                          width: 150,
+                          height: 150,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
                             color: colorScheme.onPrimary,
-                            size: 24,
+                            image: DecorationImage(
+                              fit: BoxFit.fill,
+                              image: NetworkImage(user['avatar']),
+                            ),
                           ),
-                          onPressed: () {
-                            AlertConfirmDialog.show(
-                              context: context,
-                              title: i10n.logoutConfirmTitle,
-                              message: i10n.logoutConfirmMessage,
-                              onPressedRight: () {
-                                showDialog(
-                                  context: context,
-                                  barrierColor: colorScheme.onBackground,
-                                  builder: (context) => LoadingIndicator(
-                                    height: indicatorHeight,
-                                  ),
-                                );
-
-                                SharedPreferences.getInstance().then((value) {
-                                  value.remove('user');
-                                });
-
-                                Timer(const Duration(seconds: 3), () {
-                                  Navigator.pushNamedAndRemoveUntil(
-                                    context,
-                                    '/login',
-                                    (route) => false,
-                                  );
-                                });
-                              },
-                              onPressedLeft: () {
-                                Navigator.pop(context, false);
-                              },
-                            );
-                          },
                         ),
-                      ),
-                    ],
+                        const SizedBox(
+                          height: 30,
+                        ),
+                        SAText(
+                          text: user['name'],
+                          style: textTheme.displaySmall!.copyWith(
+                            color: colorScheme.onPrimary,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 200,
+                        ),
+                        Container(
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Theme.of(context)
+                                .colorScheme
+                                .primary
+                                .withOpacity(0.1),
+                          ),
+                          child: BlocListener<AuthBloc, AuthState>(
+                            listener: (context, state) {
+                              if (state is LogoutSuccess) {
+                                Navigator.pushNamedAndRemoveUntil(
+                                  context,
+                                  '/login',
+                                  (route) => false,
+                                );
+                              }
+                            },
+                            child: BlocBuilder<AuthBloc, AuthState>(
+                              builder: (context, state) {
+                                return SAButton.icon(
+                                  child: Icon(
+                                    Icons.logout,
+                                    color: colorScheme.onPrimary,
+                                    size: 24,
+                                  ),
+                                  onPressed: () {
+                                    AlertConfirmDialog.show(
+                                      context: context,
+                                      title: i10n.logoutConfirmTitle,
+                                      message: i10n.logoutConfirmMessage,
+                                      onPressedRight: () {
+                                        loadingIndicator.show(
+                                            context: context,
+                                            height: indicatorHeight);
+                                        BlocProvider.of<AuthBloc>(context).add(
+                                          const LogoutEvent(),
+                                        );
+                                      },
+                                      onPressedLeft: () {
+                                        Navigator.pop(context, false);
+                                      },
+                                    );
+                                  },
+                                );
+                              },
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-              ),
+              );
+            }
+            return SAIndicator(
+              height: indicatorHeight,
             );
-          }
-          return LoadingIndicator(
-            height: indicatorHeight,
-          );
-        });
+          }),
+    );
   }
 }
