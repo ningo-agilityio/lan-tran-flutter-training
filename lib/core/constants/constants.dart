04e2ac7f6e73939331471c1c04745a5dd0e48d42
lantrannk <lantrannk@gmail.com>
2023-05-22 05:49:22 +0700

    define constant for services

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -1,1 +1,7 @@
 const String apiUrl = 'https://63ab8e97fdc006ba60609b9b.mockapi.io/endpoint';
+
+const List<String> items = [
+  'Back',
+  'Neck & Shoulders',
+  'Non-Invasive Body Contouring',
+];
