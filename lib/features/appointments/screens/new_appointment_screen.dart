b993e32d14f115065fa54ade6c8f1a71d4e685dd
lantrannk <lantrannk@gmail.com>
2023-05-22 17:26:33 +0700

    load user in bloc

--- lib/features/appointments/screens/new_appointment_screen.dart
+++ lib/features/appointments/screens/new_appointment_screen.dart
@@ -6,7 +6,6 @@ import '../../../core/constants/assets.dart';
 import '../../../core/constants/constants.dart';
 import '../../../core/generated/l10n.dart';
 import '../../../core/storage/appointment_storage.dart';
-import '../../../core/storage/user_storage.dart';
 import '../../../core/utils.dart';
 import '../../../core/widgets/buttons.dart';
 import '../../../core/widgets/date_picker.dart';
@@ -17,6 +16,7 @@ import '../../../core/widgets/input.dart';
 import '../../../core/widgets/snack_bar.dart';
 import '../../../core/widgets/text.dart';
 import '../../../core/widgets/time_picker.dart';
+import '../../auth/model/user.dart';
 import '../bloc/appointment_bloc.dart';
 import '../model/appointment.dart';
 
@@ -41,18 +41,6 @@ class _NewAppointmentScreenState extends State<NewAppointmentScreen> {
   late DateTime endTime = autoAddHalfHour(startTime);
 
   String? selectedValue;
-  late Map<String, dynamic> user;
-
-  @override
-  void initState() {
-    super.initState();
-
-    UserStorage.getUser().then((value) {
-      setState(() {
-        user = value;
-      });
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -61,7 +49,7 @@ class _NewAppointmentScreenState extends State<NewAppointmentScreen> {
     final double indicatorHeight = MediaQuery.of(context).size.height / 2;
 
     return BlocProvider<AppointmentBloc>(
-      create: (context) => AppointmentBloc(),
+      create: (context) => AppointmentBloc()..add(UserLoad()),
       child: Scaffold(
         appBar: AppBar(
           title: SAText.appBarTitle(
@@ -83,7 +71,16 @@ class _NewAppointmentScreenState extends State<NewAppointmentScreen> {
           padding: const EdgeInsets.all(15),
           child: Center(
             child: BlocConsumer<AppointmentBloc, AppointmentState>(
-              listener: (context, state) {
+              listener: (ctx, state) {
+                if (state is UserLoadError) {
+                  SASnackBar.show(
+                    context: context,
+                    message: S.of(context).addSuccess,
+                    isSuccess: true,
+                  );
+
+                  Navigator.pop(context);
+                }
                 if (state is AppointmentAdding) {
                   loadingIndicator.show(
                     context: context,
@@ -115,175 +112,180 @@ class _NewAppointmentScreenState extends State<NewAppointmentScreen> {
                 }
               },
               builder: (context, state) {
-                return Column(
-                  children: [
-                    const SizedBox(height: 12),
-                    Text(
-                      user['name'],
-                      style: textTheme.titleLarge,
-                    ),
-                    const SizedBox(height: 12),
-                    DatePicker(
-                        dateTime: dateTime,
-                        onPressed: () async {
-                          final DateTime? date = await showDatePicker(
-                            context: context,
-                            initialDate: dateTime,
-                            firstDate: DateTime.now(),
-                            lastDate: DateTime(dateTime.year + 5),
-                          );
-                          if (date != null && date != dateTime) {
-                            setState(() {
-                              dateTime = date;
-                              startTime = setDateTime(
-                                dateTime,
-                                TimeOfDay.fromDateTime(startTime),
-                              );
-                              endTime = setDateTime(
-                                dateTime,
-                                TimeOfDay.fromDateTime(endTime),
-                              );
-                            });
-                          }
-                        }),
-                    const SizedBox(height: 12),
-                    TimePicker(
-                      startTime: startTime,
-                      endTime: endTime,
-                      onStartTimePressed: () async {
-                        final TimeOfDay? time = await showTimePicker(
-                          context: context,
-                          initialTime: TimeOfDay.fromDateTime(startTime),
-                        );
-                        final List<Appointment> appointments =
-                            await AppointmentStorage.getAppointments();
-                        if (time != null) {
-                          final DateTime tempStartTime =
-                              setDateTime(dateTime, time);
-
-                          if (isBeforeNow(tempStartTime)) {
-                            SASnackBar.show(
+                if (state is UserLoaded) {
+                  final User user = state.user;
+                  return Column(
+                    children: [
+                      const SizedBox(height: 12),
+                      Text(
+                        user.name,
+                        style: textTheme.titleLarge,
+                      ),
+                      const SizedBox(height: 12),
+                      DatePicker(
+                          dateTime: dateTime,
+                          onPressed: () async {
+                            final DateTime? date = await showDatePicker(
                               context: context,
-                              message: S.of(context).invalidStartTimeError,
-                              isSuccess: false,
+                              initialDate: dateTime,
+                              firstDate: DateTime.now(),
+                              lastDate: DateTime(dateTime.year + 5),
                             );
-                          } else if (time !=
-                              TimeOfDay.fromDateTime(startTime)) {
-                            if (isFullAppointments(
-                                appointments, tempStartTime)) {
+                            if (date != null && date != dateTime) {
+                              setState(() {
+                                dateTime = date;
+                                startTime = setDateTime(
+                                  dateTime,
+                                  TimeOfDay.fromDateTime(startTime),
+                                );
+                                endTime = setDateTime(
+                                  dateTime,
+                                  TimeOfDay.fromDateTime(endTime),
+                                );
+                              });
+                            }
+                          }),
+                      const SizedBox(height: 12),
+                      TimePicker(
+                        startTime: startTime,
+                        endTime: endTime,
+                        onStartTimePressed: () async {
+                          final TimeOfDay? time = await showTimePicker(
+                            context: context,
+                            initialTime: TimeOfDay.fromDateTime(startTime),
+                          );
+                          final List<Appointment> appointments =
+                              await AppointmentStorage.getAppointments();
+                          if (time != null) {
+                            final DateTime tempStartTime =
+                                setDateTime(dateTime, time);
+
+                            if (isBeforeNow(tempStartTime)) {
                               SASnackBar.show(
                                 context: context,
-                                message: S.of(context).fullAppointmentsError,
+                                message: S.of(context).invalidStartTimeError,
                                 isSuccess: false,
                               );
-                            } else {
+                            } else if (time !=
+                                TimeOfDay.fromDateTime(startTime)) {
+                              if (isFullAppointments(
+                                  appointments, tempStartTime)) {
+                                SASnackBar.show(
+                                  context: context,
+                                  message: S.of(context).fullAppointmentsError,
+                                  isSuccess: false,
+                                );
+                              } else {
+                                setState(() {
+                                  startTime = tempStartTime;
+                                  endTime = autoAddHalfHour(startTime);
+                                });
+                              }
+                            }
+                          }
+                        },
+                        onEndTimePressed: () async {
+                          final TimeOfDay? time = await showTimePicker(
+                            context: context,
+                            initialTime: TimeOfDay.fromDateTime(endTime),
+                          );
+                          if (time != null) {
+                            final DateTime tempEndTime =
+                                setDateTime(dateTime, time);
+                            if (!isAfterStartTime(startTime, tempEndTime)) {
+                              SASnackBar.show(
+                                context: context,
+                                message: S.of(context).invalidEndTimeError,
+                                isSuccess: false,
+                              );
+                            } else if (time !=
+                                TimeOfDay.fromDateTime(endTime)) {
                               setState(() {
-                                startTime = tempStartTime;
-                                endTime = autoAddHalfHour(startTime);
+                                endTime = tempEndTime;
                               });
                             }
                           }
-                        }
-                      },
-                      onEndTimePressed: () async {
-                        final TimeOfDay? time = await showTimePicker(
-                          context: context,
-                          initialTime: TimeOfDay.fromDateTime(endTime),
-                        );
-                        if (time != null) {
-                          final DateTime tempEndTime =
-                              setDateTime(dateTime, time);
-                          if (!isAfterStartTime(startTime, tempEndTime)) {
+                        },
+                      ),
+                      const SizedBox(height: 12),
+                      Dropdown(
+                          items: items,
+                          selectedValue: selectedValue,
+                          onChanged: (value) {
+                            setState(() {
+                              selectedValue = value;
+                            });
+                          }),
+                      const SizedBox(height: 12),
+                      Input(
+                        controller: descpController,
+                        text: S.of(context).description,
+                        focusNode: descpFocusNode,
+                        onEditCompleted: () {
+                          FocusScope.of(context).unfocus();
+                        },
+                        color: colorScheme.secondaryContainer,
+                        maxLines: 4,
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            width: 1,
+                            color: colorScheme.secondaryContainer,
+                          ),
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                      ),
+                      const SizedBox(height: 12),
+                      ElevatedButton(
+                        onPressed: () {
+                          if (isBreakTime(startTime, endTime)) {
                             SASnackBar.show(
                               context: context,
-                              message: S.of(context).invalidEndTimeError,
+                              message: S.of(context).breakTimeConflictError,
                               isSuccess: false,
                             );
-                          } else if (time != TimeOfDay.fromDateTime(endTime)) {
-                            setState(() {
-                              endTime = tempEndTime;
-                            });
-                          }
-                        }
-                      },
-                    ),
-                    const SizedBox(height: 12),
-                    Dropdown(
-                        items: items,
-                        selectedValue: selectedValue,
-                        onChanged: (value) {
-                          setState(() {
-                            selectedValue = value;
-                          });
-                        }),
-                    const SizedBox(height: 12),
-                    Input(
-                      controller: descpController,
-                      text: S.of(context).description,
-                      focusNode: descpFocusNode,
-                      onEditCompleted: () {
-                        FocusScope.of(context).unfocus();
-                      },
-                      color: colorScheme.secondaryContainer,
-                      maxLines: 4,
-                      border: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          width: 1,
-                          color: colorScheme.secondaryContainer,
-                        ),
-                        borderRadius: BorderRadius.circular(8),
-                      ),
-                    ),
-                    const SizedBox(height: 12),
-                    ElevatedButton(
-                      onPressed: () {
-                        if (isBreakTime(startTime, endTime)) {
-                          SASnackBar.show(
-                            context: context,
-                            message: S.of(context).breakTimeConflictError,
-                            isSuccess: false,
-                          );
-                        } else if (isClosedTime(startTime, endTime)) {
-                          SASnackBar.show(
-                            context: context,
-                            message: S.of(context).closedTimeError,
-                            isSuccess: false,
-                          );
-                        } else if (selectedValue == null) {
-                          SASnackBar.show(
-                            context: context,
-                            message: S.of(context).emptyServicesError,
-                            isSuccess: false,
-                          );
-                        } else {
-                          context.read<AppointmentBloc>().add(
-                                AppointmentAdd(
-                                  appointment: Appointment(
-                                    userId: user['id'],
-                                    date: dateTime,
-                                    startTime: startTime,
-                                    endTime: endTime,
-                                    services: selectedValue!,
-                                    description: descpController.text == ''
-                                        ? S.of(context).defaultDescription
-                                        : descpController.text,
+                          } else if (isClosedTime(startTime, endTime)) {
+                            SASnackBar.show(
+                              context: context,
+                              message: S.of(context).closedTimeError,
+                              isSuccess: false,
+                            );
+                          } else if (selectedValue == null) {
+                            SASnackBar.show(
+                              context: context,
+                              message: S.of(context).emptyServicesError,
+                              isSuccess: false,
+                            );
+                          } else {
+                            context.read<AppointmentBloc>().add(
+                                  AppointmentAdd(
+                                    appointment: Appointment(
+                                      userId: user.id,
+                                      date: dateTime,
+                                      startTime: startTime,
+                                      endTime: endTime,
+                                      services: selectedValue!,
+                                      description: descpController.text == ''
+                                          ? S.of(context).defaultDescription
+                                          : descpController.text,
+                                    ),
                                   ),
-                                ),
-                              );
-                        }
-                      },
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: colorScheme.primary,
-                      ),
-                      child: Text(
-                        S.of(context).createAppointmentButton,
-                        style: textTheme.labelMedium!.copyWith(
-                          color: colorScheme.onPrimary,
+                                );
+                          }
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: colorScheme.primary,
+                        ),
+                        child: Text(
+                          S.of(context).createAppointmentButton,
+                          style: textTheme.labelMedium!.copyWith(
+                            color: colorScheme.onPrimary,
+                          ),
                         ),
                       ),
-                    ),
-                  ],
-                );
+                    ],
+                  );
+                }
+                return Container();
               },
             ),
           ),
